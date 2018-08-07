17e4a41a38a7e8d424e5ddaa8b60da71c15799e7
YeYuJie <37700865+YeYuJie@users.noreply.github.com>
2018-08-07 11:02:08 +0800

    背包金钱

---
+++ Assets/Scripts/backpack/DisplayMoney.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayMoney : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.Find("moneyText").GetComponent<TextMesh>().text = GlobalVariable.money.ToString();
+	}
+}
